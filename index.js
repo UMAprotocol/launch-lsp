f00d71a397a44bd9a5182dc395198b1a38a773b2
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2022-07-28 09:36:03 -0700

    fix: remove extra deployment (#24)
    * fix: remove extra deployment
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    * refactor: add space
    
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    Co-authored-by: Pablo Maldonado <pablo@umaproject.org>

--- index.js
+++ index.js
@@ -180,16 +180,11 @@ const earlyExpiration = argv.enableEarlyExpiration ? argv.enableEarlyExpiration 
   await lspCreator.methods.createLongShortPair(lspParams).call(transactionOptions);
 
   // Since the simulated transaction succeeded, send the real one to the network.
-  if (!argv.simulate) {
-    const { transactionHash } = await lspCreator.methods.createLongShortPair(lspParams).send(transactionOptions);
-    console.log("Deployed in transaction:", transactionHash);
-  }
-
   let address;
   if (!argv.simulate) {
     const { transactionHash } = await lspCreator.methods.createLongShortPair(lspParams).send(transactionOptions);
     address = (await lspCreator.getPastEvents("CreatedLongShortPair"))[0].returnValues.longShortPair;
-    console.log("Deployed in transaction:", transactionHash, "LSP Address:", address);
+    console.log("Deployed in transaction:", transactionHash, " LSP Address:", address);
   }
 
   // Set the FPL parameters.
